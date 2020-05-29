de387ce99cd15d4d9a093bd921d33aa3a126b38d
Rootbro92 <Rootbro92@gmail.com>
2020-05-30 01:59:53 +0900

    delete StoryBoard

--- SimpleGithubApp/AppDelegate.swift
+++ SimpleGithubApp/AppDelegate.swift
@@ -14,7 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupRootView()
         return true
+    }
+    
+    private func setupRootView() {
+      let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UINavigationController(rootViewController: UserListViewController())
+      window.backgroundColor = .white
+      window.makeKeyAndVisible()
+      self.window = window
     }
 }
 
